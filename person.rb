bb32be23150f14a65b2a73a89024705e57794341
beteland123 <beteland23@gmail.com>
2023-08-15 03:42:22 +0300

    fix rubcop

--- person.rb
+++ person.rb
@@ -1,33 +1,29 @@
-
 class Person
-    attr_reader :id
-    attr_accessor :name
-    attr_accessor :age
-    
-    def initialize( age, name = "Unknown", parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-    
-    
-    private
+  attr_reader :id
+  attr_accessor :name, :age
 
-    def of_age?
-      @age >= 18
-    end
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age.to_i
+    @parent_permission = parent_permission
+  end
 
-    public
+  private
 
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
 
-person = Person.new(15, 'John', false)
-puts person.can_use_services? 
+person = Person.new(15, name: 'John', parent_permission: false)
+puts person.can_use_services?
 
-person2 = Person.new(48, 'selam')
+person2 = Person.new(48, name: 'selam')
 puts person2.can_use_services?
--- student.rb
+++ student.rb
@@ -1,16 +1,15 @@
-require "./person.rb"
+require_relative 'person'
 
-class Student <  Person
-    def initialize (classroom, age, name = "Unknown", parent_permission = true)
-        super(age, name,  parent_permission)
-        @classroom = classroom
-    end
+# Creates class Student (extends Person)
+class Student < Person
+  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name:, parent_permission:)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
-  
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
 end
-
-student = Student.new(  'c1',32, 'Helen')
+student = Student.new(16, 'Math Class', name: 'Alice', parent_permission: true)
 puts student.play_hooky
