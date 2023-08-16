f640da107f164c00afe96be594a3a29e78f51f58
beteland123 <beteland23@gmail.com>
2023-08-16 21:51:37 +0300

    add book class

---
+++ book.rb
@@ -0,0 +1,12 @@
+require_relative 'rental'
+
+class Book
+  attr_reader :rental
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental = []
+  end
+end
