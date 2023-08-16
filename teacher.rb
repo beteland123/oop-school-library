4313903e9f5719286c64a2ee60d0f94d25de175e
beteland123 <beteland23@gmail.com>
2023-08-16 22:05:51 +0300

    fix rubocop

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 # Creates class Teacher (extends Person)
 class Teacher < Person
   def initialize(specialization, age, name: 'Unknown', parent_permission: true)
-    super(age, name:name, parent_permission:parent_permission)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
