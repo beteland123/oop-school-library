e18f4c8cae1258504cc911091793399c580ba9e8
beteland123 <beteland23@gmail.com>
2023-08-16 21:54:42 +0300

    update student class for classrom relationship

--- student.rb
+++ student.rb
@@ -1,13 +1,20 @@
 require_relative 'person'
-
+require_relative 'classroom'
 # Creates class Student (extends Person)
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+    super(age, name:name, parent_permission:parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
