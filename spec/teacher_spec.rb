15d340317bbaba28474cd0d381f2dc144cea5c81
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-25 03:10:18 -0600

    add teacher test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
-RSpec.describe Teacher do
-  describe '#initialize' do
-    it 'creates a teacher object with specialization, age, name, and parent permission' do
-      teacher = Teacher.new('Math', 40, name: 'John Doe', parent_permission: true)
-      expect(teacher.instance_variable_get(:@specialization)).to eq 'Math'
-      expect(teacher.age).to eq 40
-      expect(teacher.name).to eq 'John Doe'
-      expect(teacher.parent_permission).to eq true
+describe Teacher do
+  context 'when initializing' do
+    it 'sets the specialization attribute' do
+      teacher = Teacher.new('Math', 35)
+      expect(teacher.instance_variable_get(:@specialization)).to eq('Math')
+    end
+
+    it 'inherits age, name, and parent_permission from Person' do
+      teacher = Teacher.new('Math', 35, name: 'Ms. Smith', parent_permission: true)
+      expect(teacher.age).to eq(35)
+      expect(teacher.name).to eq('Ms. Smith')
+      expect(teacher.parent_permission).to be(true)
     end
   end
 
   describe '#can_use_services?' do
-    it 'returns true indicating the teacher can use services' do
-      teacher = Teacher.new('History', 45)
-      expect(teacher.can_use_services?).to eq true
+    it 'returns true' do
+      teacher = Teacher.new('Science', 42)
+      expect(teacher.can_use_services?).to be(true)
     end
   end
 end
